4c7e1bd0d43665753440911bf97a3e768c93bc7b
KTran <tranthienkhoi@gmail.com>
2020-11-06 21:57:53 +0700

    Correct fieldname

--- src/Api/GroupApi.ts
+++ src/Api/GroupApi.ts
@@ -79,7 +79,7 @@ class GroupApi extends AbstractApi {
 
   public updatePolicy(groupId: string, meta: string[]): AxiosPromise<IUserGroup> {
     const query: string = `mutation updatePolicy($groupId: String!, $meta: [String]!) {
-      updatePolicy(group: $groupId, meta: $meta) {
+      updatePolicy(groupId: $groupId, meta: $meta) {
         name
         id
         isAdmin

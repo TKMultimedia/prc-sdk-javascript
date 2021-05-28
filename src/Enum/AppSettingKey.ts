da29b03ac47bd02562930e6386f7e7bc25c4aca0
KTran <tranthienkhoi@gmail.com>
2021-05-28 20:37:54 +0700

    App setting key

--- src/Enum/AppSettingKey.ts
+++ src/Enum/AppSettingKey.ts
@@ -1,7 +1,8 @@
 enum AppSettingKey {
   WelcomeMessage = 'welcome_message',
   EnablePayment = 'enabled_payment',
-  AllowSSO = 'allow_sso'
+  AllowSSO = 'allow_sso',
+  ScoutRequestFee = 'scout_request_fee'
 }
 
 export default AppSettingKey;

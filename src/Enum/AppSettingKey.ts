08ef9b16d51e30fb86d33f0d00c7acff61fe3f63
KTran <tranthienkhoi@gmail.com>
2021-06-03 19:35:51 +0700

    Added new key

--- src/Enum/AppSettingKey.ts
+++ src/Enum/AppSettingKey.ts
@@ -2,7 +2,8 @@ enum AppSettingKey {
   WelcomeMessage = 'welcome_message',
   EnablePayment = 'enabled_payment',
   AllowSSO = 'allow_sso',
-  ScoutRequestFee = 'scout_request_fee'
+  ScoutRequestFee = 'scout_request_fee',
+  SystemNotificationEmailList = 'system_email_list'
 }
 
 export default AppSettingKey;
